d70b877faa4cb253fc7ab6e9752cf2497ce8a4dd
xShir0s <mtodys2006@gmail.com>
2024-10-10 09:24:19 +0200

    Dodanie plików strony

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="grid-container">
+        <div className="box">
+            <img src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+            <a className="Title" > Apple Airpods Pro 2. generacji
+                (USB‑C)</a>
+            <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+            <p className="Text">Łączność: True Wireless
+                Budowa słuchawek: Dokanałowe
+                Mikrofon: Posiada, przy słuchawce
+                Redukcja hałasu: Aktywna - ANC</p>
+            <button>
+                🛒
+            </button>
+        </div>
+
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+
+          </div>
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+          </div>
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+          </div>
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+          </div>
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+          </div>
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+          </div>
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+          </div>
+          <div className="box">
+              <img
+                  src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/9/pr_2024_9_9_23_20_40_927_00.jpg"/>
+              <a className="Title"> Apple Airpods Pro 2. generacji
+                  (USB‑C)</a>
+              <p className="Review">⭐⭐⭐⭐⭐ (1164) </p>
+              <p className="Text">Łączność: True Wireless
+                  Budowa słuchawek: Dokanałowe
+                  Mikrofon: Posiada, przy słuchawce
+                  Redukcja hałasu: Aktywna - ANC</p>
+              <button>
+                  🛒
+              </button>
+          </div>
+      </div>
   );
 }
 
